d5cfefd452f1734df43ec7dae9dd9755dbc713c6
David <david.kaiblinger105@gmail.com>
2021-03-14 18:34:13 +0100

    Rough UI Element placement, some demo functionality for exercise 3

---
+++ MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using SWE2_TOURPLANNER.Annotations;
+using SWE2_TOURPLANNER.Model;
+
+namespace SWE2_TOURPLANNER
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<TourEntry> Data { get; }
+            = new ObservableCollection<TourEntry>();
+
+        public string TourName { get; set; }
+        public string TourDescription { get; set; }
+        public string RouteInformation { get; set; }
+        public string TourDistance { get; set; }
+        public RelayCommand AddCommand { get; }
+
+
+        private bool _isUsernameFocused;
+        public bool IsUsernameFocused
+        {
+            get => _isUsernameFocused;
+            set
+            {
+                // it needs to flip, else it does not execute properly, so let's reset here
+                _isUsernameFocused = false;
+                OnPropertyChanged();
+                _isUsernameFocused = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            AddCommand = new RelayCommand((_) =>
+            {
+                Data.Add(new TourEntry(this.TourName,this.TourDescription, this.RouteInformation, this.TourDistance));
+                TourName = String.Empty;
+                TourDescription = string.Empty;
+                RouteInformation = string.Empty;
+                TourDistance = string.Empty;
+                OnPropertyChanged(nameof(TourDescription));
+                OnPropertyChanged("RouteInformation");
+                OnPropertyChanged("TourDistance");
+                IsUsernameFocused = true;
+            });
+            IsUsernameFocused = true;
+
+            // real data to add (not design data)
+            Data.Add(new TourEntry("Gute Tour","Eine schöne Tour", "Die Route ist hart und schwer", "Es ist sehr lang"));
+            Data.Add(new TourEntry("Schlechte Tour","Eine hässliche Tour", "Die Route ist leicht und leicht", "Es ist sehr kurz"));
+        }
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Model/TourEntry.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SWE2_TOURPLANNER.Model
+{
+    public class TourEntry : INotifyPropertyChanged
+    {
+        private string _tourName;
+        private string _tourDescription;
+        private string _routeInformation;
+        private string _tourDistance;
+
+        public TourEntry(string tourName, string tourDescription, string routeInformation, string tourDistance)
+        {
+            this._tourName = tourName;
+            this._tourDescription = tourDescription;
+            this._routeInformation = routeInformation;
+            this._tourDistance = tourDistance;
+        }
+
+        public string TourName
+        {
+            get => this._tourName;
+            set
+            {
+                this._tourName = value;
+                this.OnPropertyChanged(); // using CallerMemberName
+            }
+        }
+        public string TourDescription
+        {
+            get => this._tourDescription;
+            set
+            {
+                this._tourDescription = value;
+                this.OnPropertyChanged(); // using CallerMemberName
+            }
+        }
+
+        public string RouteInformation
+        {
+            get => this._routeInformation;
+            set
+            {
+                this._routeInformation = value;
+                this.OnPropertyChanged(nameof(RouteInformation));
+            }
+        }
+
+        public string TourDistance
+        {
+            get => this._tourDistance;
+            set
+            {
+                this._tourDistance = value;
+                this.OnPropertyChanged(nameof(TourDescription));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
